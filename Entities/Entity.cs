f609935c9f4e85e1a3ec6862aa278d4453fd38c0
öner taş <tasoner@gmail.com>
2026-03-24 15:45:14 +0300

    erere

--- Entities/Entity.cs
+++ Entities/Entity.cs
@@ -3,7 +3,7 @@
     public abstract class Entity
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string Code { get; set; } = string.Empty;
+        public string? Code { get; set; }
         public bool IsDeteled { get; set; }
     }
 }
