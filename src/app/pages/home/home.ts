facd80c7ccaecd1533beb89335fabd4fb119e973
öner taş <tasoner@gmail.com>
2026-03-26 15:11:05 +0300

    erer

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,7 +1,8 @@
-import { Component, inject, OnInit,} from '@angular/core';
-import { Blank } from "../../components/blank/blank";
-import { Section } from "../../components/section/section";
-
+import { Component, inject, OnInit } from '@angular/core';
+import { Blank } from '../../components/blank/blank';
+import { Section } from '../../components/section/section';
+import { HttpApiService } from '../../services/http-api-service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +10,23 @@ import { Section } from "../../components/section/section";
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home implements OnInit{
-  ngOnInit(): void {
-   
+export class Home implements OnInit {
+  http = inject(HttpClient);
+  ngOnInit(): void {}
+  sonuc: any;
+
+  ask() {
+  const url = 'https://n8n.onertas.com/webhook/mevzuat-soru';
+  const body = {
+    pdfUrl: 'https://www.mevzuat.gov.tr/MevzuatMetin/yonetmelik/7.5.42353.pdf',
+    soru: 'Tabela standartları nelerdir?',
+  };
+
+  // withCredentials: false yaparak sunucunun 'true' dönme zorunluluğunu ortadan kaldırın
+  this.http.post<any>(url, body, { withCredentials: true})
+    .subscribe({
+      next: (res) => { this.sonuc = res; console.log('Başarılı:', res); },
+      error: (err) => { console.error('Hata:', err); }
+    });
   }
-
-
-
-
-
-
 }
